dabecbba2f80b1ac00c1aa018f9ec668ba8c7087
Fahad Al Rabbani Jillani <FahadAlRabbani@users.noreply.github.com>
2021-02-20 11:39:30 +0000

    Add domain models for checkout

---
+++ CheckoutKata/Models/Offer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Models
+{
+    public class Offer
+    {
+        public string sku { get; set; }
+        public int count { get; set; }
+        public float offerPrice { get; set; }
+
+        public Offer(string _sku, int _count, float _offerPrice)
+        {
+            sku = _sku;
+            count = _count;
+            offerPrice = _offerPrice;
+        }
+    }
+}
