0b7aab228e3a3939f6ec2ff48179321d2e5b0069
Fahad Al Rabbani Jillani <FahadAlRabbani@users.noreply.github.com>
2021-02-20 12:12:37 +0000

    Add Checout and Offers interface and implementation

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckoutKata
+{
+    public class Checkout : ICheckout
+    {
+        public List<string> BasketList = new List<string>();
+
+        private IOffers _offers;
+
+        public void Configure(IOffers offers)
+        {
+            _offers = offers;
+        }
+
+        public void Scan(string sku)
+        {
+            BasketList.Add(sku);
+        }
+
+        public void Remove(string sku)
+        {
+            BasketList.Remove(sku);
+        }
+
+        public void Empty()
+        {
+            BasketList.Clear();
+        }
+
+        public float Subtotal()
+        {
+            return BasketList.Distinct().Sum(s => BasketList.Count(t => t == s) * _offers.GetProductPrice(s));
+        }
+
+        public float Savings()
+        {
+            var totalSavings = 0.0f;
+            var distinctBasket = BasketList.Distinct();
+            foreach (var sku in distinctBasket)
+            {
+                var deals = _offers.GetDealForProduct(sku).OrderByDescending(d => d.count).ToList();
+
+                if (!deals.Any()) continue;
+
+                var countOfItems = BasketList.Count(s => s == sku);
+
+                var basePrice = _offers.GetProductPrice(sku);
+
+                var originalPrice = countOfItems * basePrice;
+
+                var savings = 0.0f;
+                foreach (var deal in deals)
+                {
+                    if (countOfItems < deal.count) continue;
+                    savings += deal.offerPrice;
+                    countOfItems -= deal.count;
+                    break;
+                }
+
+                savings += countOfItems * basePrice;
+                totalSavings = originalPrice - savings;
+            }
+
+            return totalSavings;
+        }
+
+        public float Total()
+        {
+            return Subtotal() - Savings();
+        }
+    }
+
+    public interface ICheckout
+    {
+        void Configure(IOffers offers);
+        void Scan(string sku);
+        void Remove(string sku);
+        void Empty();
+        float Subtotal();
+        float Savings();
+        float Total();
+    }
+}
---
+++ CheckoutKata/Offers.cs
@@ -0,0 +1,44 @@
+﻿using CheckoutKata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckoutKata
+{
+    public class Offers : IOffers
+    {
+        public List<Product> _Products = new List<Product>();
+
+        public List<Offer> _Deals = new List<Offer>();
+
+        public void AddProduct(string sku, float price)
+        {
+            if (sku.Length > 0 && price > 0.0f)
+                _Products.Add(new Product(sku, price));
+        }
+
+        public void AddDeal(string sku, int count, float price)
+        {
+            if (sku.Length > 0 && count > 0 && price > 0.0f)
+                _Deals.Add(new Offer(sku, count, price));
+        }
+
+        public float GetProductPrice(string sku)
+        {
+            return _Products.Find(p => p.sku == sku).price;
+        }
+
+        public IEnumerable<Offer> GetDealForProduct(string sku)
+        {
+            return _Deals.FindAll(deal => deal.sku == sku);
+        }
+    }
+
+    public interface IOffers
+    {
+        void AddProduct(string sku, float price);
+        void AddDeal(string sku, int count, float price);
+        float GetProductPrice(string sku);
+        IEnumerable<Offer> GetDealForProduct(string sku);
+    }
+}
